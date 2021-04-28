56bffa552810d1132543adc158a54bc3f9eb05b0
Pietro Ballarin <pietroballarin@outlook.com>
2021-04-28 23:53:56 +0200

    single product page added

--- routes/products.js
+++ routes/products.js
@@ -1,18 +1,15 @@
 const express = require("express");
 const Product = require("../models/Product");
-const Category = require('../models/Category');
 const User = require("../models/User.model");
 const router = express.Router();
 const ensureLogin = require('connect-ensure-login');
 const { uploader, cloudinary } = require("../config/cloudinary");
 
 
+
 router.post('/', uploader.single('photo'), (req, res) => {
-  const { title, description, condition, price, categories, creator} = req.body;
-  // const imgPath = req.file.path;
-  console.log(req.file)
+  const { title, description, condition, price, categories, creator, userEmail, address} = req.body;
   if (req.file.path) {
-    console.log('image');
     Product.create({
       title: title,
       description: description,
@@ -20,14 +17,13 @@ router.post('/', uploader.single('photo'), (req, res) => {
       price: price,
       categories: categories,
       creator: req.session.passport.user,
-      imgPath: req.file.path
+      imgPath: req.file.path,
+      userEmail: userEmail,
+      address: address
     })
       .then(productAdd => {
         console.log(productAdd)
-        //   Category.find()
-        //   .then(categories => {
           res.redirect('/')
-      // })
       })   
   } else {
     Product.create({
@@ -36,34 +32,16 @@ router.post('/', uploader.single('photo'), (req, res) => {
       condition: condition,
       price: price,
       categories: categories,
-      creator: req.session.passport.user
+      creator: req.session.passport.user,
+      userEmail: userEmail,
+      address: address
     })
       .then(productAdd => {
         console.log('pathNoImage', productAdd)
-        //   Category.find()
-        //   .then(categories => {
           res.redirect('/')
-      // })
       })    
   }
  
 });
-
-// router.get('/product/delete/:id', (req, res, next) =>{
-//   Product.findByIdAndDelete(req.params.id)
-// })
-
-
-// router.get('/add', (req, res) => {
-//   res.render('products/new.hbs')
-// })
-
-router.get('/products/new', ensureLogin.ensureLoggedIn(), (req, res, next) => {
-  Category.find({})
-    .then(categories => {
-      res.render('products/new', { user: req.user, categoryList: categories})
-    })
-  });
-
 
 module.exports = router;
