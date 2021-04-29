194b42ce342035375e657e56400e78c3f96d7c1e
Pietro Ballarin <pietroballarin@outlook.com>
2021-04-29 15:05:26 +0200

    some fixes error messages

--- routes/index.js
+++ routes/index.js
@@ -3,69 +3,80 @@ const Product = require("../models/Product");
 const User = require("../models/User.model");
 const ensureLogin = require('connect-ensure-login');
 
-/* GET home page */
+
 router.get('/', (req, res, next) => {
   const categories = ['Select a Category', 'Books', 'Clothes', 'Cars', 'Collectibles & Antiquities', 'Electronics', 'Furniture', 'Sport', 'Bicycles']
   Product.find()
-   .then(products=> {
-    res.render('index', {categories, productInfo: products}) 
-})
-  .catch(err => {
-    next(err);
-  })
+    .then(products => {
+      res.render('index', {
+        categories,
+        productInfo: products
+      })
+    })
+    .catch(err => {
+      next(err);
+    })
 });
 
 router.get('/show/:id', (req, res, next) => {
   Product.findById(req.params.id)
-  .then(product => {
+    .then(product => {
       console.log(product, "AAAAA")
-      res.render('show.hbs', {productInfo: product})
-  })
+      res.render('show.hbs', {
+        productInfo: product
+      })
+    })
 })
 
 router.get('/dashboard', ensureLogin.ensureLoggedIn(), (req, res, next) => {
   const creatorId = req.session.passport.user;
-  Product.find({creator: creatorId})
-  .then(products => {
-    res.render('dashboard', {productInfo: products})
-  })
-  .catch(err => {
-    next(err);
-  })
+  Product.find({
+      creator: creatorId
+    })
+    .then(products => {
+      res.render('dashboard', {user: req.user,
+        productInfo: products
+      })
+    })
+    .catch(err => {
+      next(err);
+    })
 })
 
-router.get('/', (req, res, next) => {
-  const { title } = req.query;
-  console.log(req.query)
+router.get('/search', (req, res, next) => {
+  const {
+    title
+  } = req.query;
   Product.find()
-  .then(products => { 
-    // console.log(products, "PRODUCTS")
-    const searchedProd = products.filter(product => {
-      return product.title.toLowerCase().includes(req.query.q.toLowerCase())
+    .then(products => {
+      const searchedProd = products.filter(product => {
+        return product.title === req.query.q;
+      })
+      console.log(searchedProd, 'aAAA')
+      res.render('index.hbs', {
+        productInfo: searchedProd
+      })
     })
-    console.log(searchedProd, 'aAAA')
-  res.render('index.hbs', { productInfo: searchedProd })
-  })
-  .catch(err => {
-    next(err);
-  })
- })
+    .catch(err => {
+      next(err);
+    })
+})
 
- router.get('/category', (req, res, next) => {
-  const { categories } = req.query.categories;
-  Product.find()
-  .then(products => { 
-    const searchedProd = products.filter(product => {
-      return product.categories.includes(req.query.categories)
-      
+router.get('/category', (req, res, next) => {
+  console.log(req.query, "QUERY")
+  console.log(req.body)
+  Product.find({
+      categories: req.query.categories
     })
-    console.log(searchedProd, "Searched PRODUCTS")
-  res.render('category.hbs', { productInfo: searchedProd })
-  })
-  .catch(err => {
-    next(err);
-  })
- })
+    .then(products => {
+      res.render('category.hbs', {
+        productInfo: products
+      })
+    })
+    .catch(err => {
+      next(err);
+    })
+})
 
 router.get('/logout', (req, res) => {
   req.logout();
@@ -74,7 +85,10 @@ router.get('/logout', (req, res) => {
 
 router.get('/', ensureLogin.ensureLoggedIn(), (req, res, next) => {
   const categories = ['Select a Category', 'Books', 'Clothes', 'Cars', 'Collectibles & Antiquities', 'Electronics', 'Furniture', 'Sport', 'Bicycles']
-  res.render('products/new', { user: req.user, categories})
+  res.render('products/new', {
+    user: req.user,
+    categories
+  })
 });
 
 module.exports = router;
